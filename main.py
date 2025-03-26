542e5db56ddb59e85c6a305e04d2cd0296277c02
aquinones154 <113639433+aquinones154@users.noreply.github.com>
2025-03-25 23:53:56 -0500

    tried to includ indigo(purple) and white not working yet

--- main.py
+++ main.py
@@ -2,40 +2,50 @@ import numpy as np
 import cv2
 import os
 
-#open camera
+# Open the camera
 cam = cv2.VideoCapture(0)
 
+# limits to check for special colors
+high_limit = 150
+low_limit = 100
 
 while True:
     ret, frame = cam.read()
+    if not ret:
+        print("Something went wrong")
+        break
+
+    #might remove this later on
     cv2.imshow('frame', frame)
     output = frame.copy()
-    rectangle = cv2.rectangle(output,(50,50),(100,100),(0,255,0),4)
-    cv2.imshow('rectangle',rectangle)
+    rectangle = cv2.rectangle(output, (50, 50), (100, 100), (0, 255, 0), 4)
+    cv2.imshow('rectangle', rectangle)
     
-    # mapping the frame to BGR(rgb but in reverse order according to opencv)
-    b = frame[:, :, 0]  #blue
-    g = frame[:, :, 1]  #green
-    r = frame[:, :, 2]  #red
+    # mapping the colors
+    b = frame[:, :, 0]  
+    g = frame[:, :, 1]  
+    r = frame[:, :, 2]  
 
-    #getting average of each color
+    # getting average of each color
     b_mean = np.mean(b)
     g_mean = np.mean(g)
     r_mean = np.mean(r)
 
-    if (b_mean > g_mean and b_mean > r_mean):
-        print("Blue")
-    if (g_mean > b_mean and g_mean > r_mean):
-        print("Green")
+    # speical colors
+    if (b_mean > high_limit and r_mean > high_limit and g_mean < low_limit):
+        print("Indigo")
+    elif (r_mean > high_limit and g_mean > high_limit and b_mean > high_limit):
+        print("White")
     else:
-        print("Red")
+        # if not speical color then return primary color
+        if b_mean > g_mean and b_mean > r_mean:
+            print("Blue")
+        elif g_mean > b_mean and g_mean > r_mean:
+            print("Green")
+        else:
+            print("Red")
 
-
-    if ret == False:
-        print("Something went wrong")
-        break
-
-    if cv2.waitKey(1) & 0xFF == ord('q'): # break the loop when q is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # break loop when user prcess 'q' 
         break
 
 cam.release()
